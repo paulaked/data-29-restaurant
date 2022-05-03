d7d1283deb62ad96d6a45ab8969cc4aaf0d698cb
osalamisparta <104364994+osalamisparta@users.noreply.github.com>
2022-05-03 18:39:25 +0100

    Order function added + tested

--- restaurant.py
+++ restaurant.py
@@ -18,11 +18,32 @@ class Table:
                 self.bill.append({'item': item, 'price': price, 'quantity': quantity})
                 return
 
+    def remove (self, item, price, quantity:1) -> list:
+        if self.bill == []:
+            return
+        else:
+            for x in self.bill:
+                iandp = list(x.values())
+                iandp = [iandp[0],iandp[2]]
+                if x['item'] == item and x['quantity'] - quantity >= 1:
+                    x.update({'item': item, 'price': price, 'quantity': (x['quantity']-quantity)})
+                    return
+                if x['item'] == item and x['quantity'] - quantity < 1:
+                    self.bill.pop(self.bill.index(x))
+                    return
+
 
 
 
 table01 = Table(5)
 table01.order("Risotto", 12.50, 2)
-table01.order("Chicken", 12.50, 2)
 table01.order("Risotto", 12.50, 2)
+print(table01.bill)
+table01.order("Chicken", 12.50, 2)
+print(table01.bill)
+table01.remove("Risotto", 12.50, 2)
+table01.remove("Risotto", 12.50, 2)
+
+
+
 print(table01.bill)
