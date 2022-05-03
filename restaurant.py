353917328e46f5b93e13e09e6daefb2cedf7b944
osalamisparta <104364994+osalamisparta@users.noreply.github.com>
2022-05-03 19:43:14 +0100

    Totals and remove added

--- restaurant.py
+++ restaurant.py
@@ -3,7 +3,7 @@ class Table:
         self.bill = []
         self.people = people
 
-    def order (self, item, price, quantity:1) -> list:
+    def order (self, item, price, quantity=1) -> list:
         if self.bill == []:
             self.bill.append({'item': item, 'price': price, 'quantity': quantity})
             return
@@ -18,32 +18,43 @@ class Table:
                 self.bill.append({'item': item, 'price': price, 'quantity': quantity})
                 return
 
+
     def remove (self, item, price, quantity:1) -> list:
         if self.bill == []:
-            return
-        else:
+            return False
+        elif self.bill != []:
             for x in self.bill:
                 iandp = list(x.values())
                 iandp = [iandp[0],iandp[2]]
                 if x['item'] == item and x['quantity'] - quantity >= 1:
                     x.update({'item': item, 'price': price, 'quantity': (x['quantity']-quantity)})
-                    return
+                    return True
                 if x['item'] == item and x['quantity'] - quantity < 1:
                     self.bill.pop(self.bill.index(x))
-                    return
+                    return True
+        else:
+            return False
 
+    def get_subtotal (self):
+        total = 0
+        if self.bill == []:
+            return 0
+        if self.bill !=[]:
+            for x in self.bill:
+                total += x['price'] * x['quantity']
+            return total
+
+    def get_total (self,service_charge = 0.1):
+        subtotal  =self.get_subtotal()
+
+        return {'Sub Total': f'£{subtotal:.2f}', 'Service Charge': f'£{subtotal*service_charge:.2f}',
+                'Total': f'£{subtotal+subtotal*service_charge:.2f}'}
 
 
 
 table01 = Table(5)
 table01.order("Risotto", 12.50, 2)
 table01.order("Risotto", 12.50, 2)
-print(table01.bill)
-table01.order("Chicken", 12.50, 2)
-print(table01.bill)
-table01.remove("Risotto", 12.50, 2)
-table01.remove("Risotto", 12.50, 2)
-
-
-
-print(table01.bill)
+table01.order("Risotto", 12.50, 2)
+table01.order("chicken", 12.50, 2)
+print(table01.get_total())
