718a7e9d48297f7f471a26109f3c9a8040a6f988
june <juneli@spartaglobal.com>
2022-05-03 16:32:21 +0100

    add the order method and pass test_order and test_order_quantitty

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,15 @@
 class Table:
-    pass
+
+    def __init__(self, diners: int):
+        self.diners = diners
+        self.bill = []
+
+    # method to append a menu item to the bill
+    def order(self, item, price, quantity = 1):
+        if {"item": item, "price": price, "quantity": quantity} not in self.bill:
+            self.bill.append({"item": item, "price": price, "quantity": quantity})
+        else:
+            self.bill.update({"quantity": quantity + 1})
+
+    
+
