c8eca6853b12c75bf5556ec73841dc831a59ea77
june <juneli@spartaglobal.com>
2022-05-03 18:01:44 +0100

    add functionality to increase quantity of items which already exist in bill based on item name

--- restaurant.py
+++ restaurant.py
@@ -3,20 +3,30 @@ class Table:
     def __init__(self, diners: int):
         self.diners = diners
         self.bill = []
+        self._total = 0
+        self._subtotal = 0
 
     # method to append a menu item to the bill
     # passes test_order and test_order_no_quantity
     def order(self, item, price, quantity = 1):
-        if {"item": item, "price": price, "quantity": quantity} not in self.bill:
-            self.bill.append({"item": item, "price": price, "quantity": quantity})
+        if len(self.bill) != 0:
+            for i in self.bill:
+                if item != i["item"]:
+                    self.bill.append({"item": item, "price": price, "quantity": quantity})
+                else:
+                    i["quantity"] += quantity
         else:
-            self.bill.update({"quantity": quantity + 1})
+            self.bill.append({"item": item, "price": price, "quantity": quantity})
 
-    # method to remove bill items
-    # passes test_remove_no_item
+
+    method to remove bill items
+    passes test_remove_no_item
     def remove(self, item, price, quantity = 1):
-        if {"item": item, "price": price, "quantity": quantity}  in self.bill:
-            self.bill.pop({"item": item, "price": price, "quantity": quantity})
+        for i in self.bill:
+            if {"item": item, "price": price, "quantity": quantity} == i:
+                self.bill.remove({"item": item, "price": price, "quantity": quantity})
         else:
             pass
 
+    def get_subtotal(self):
+        pass
