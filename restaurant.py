ae5050f57c4606f89335dd55bc06cdd6331817e8
Tatenda Mukutirwa <tatendanelson1@gmail.com>
2022-05-04 15:57:25 +0100

    Testing git push

--- restaurant.py
+++ restaurant.py
@@ -4,14 +4,13 @@ class Table:
         self.num_of_people = num_of_people
         self.bill = []
 
-    def order(self, item, price):
+    def order(self, item, price, quantity=1):
         self.item = item
         self.price = price
-
+        self.quantity
 
     def order(self):
         pass
-
 
     def remove(self):
         pass
