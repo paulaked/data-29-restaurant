93ffa3550c5ce507f33796d3397b4a836e6b6637
Durgesh Murugan <durgeshmurugan7962@gmail.com>
2022-05-04 09:40:42 +0100

    update

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,22 @@
 class Table:
-    pass
+
+    def __init__(self, number_of_people, bill):
+        self.number_of_people = number_of_people
+        self.bill = bill
+
+    def order(self, item, price, quantity):
+        bill = dict.fromkeys(['item', 'price','quantity'])
+        #orders = {"item": item, "price": price, "quantity": quantity}
+        bill.append(item,price,quantity)
+
+    def remove(self):
+
+
+
+
+
+
+
+
+
+
