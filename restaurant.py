8190c2e5ed4f995586eeb8848fe26da4fb1337bc
TatendaMukutirwa <99507770+TatendaMukutirwa@users.noreply.github.com>
2022-05-04 15:14:21 +0100

    Update restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,17 @@
 class Table:
-    pass
+
+    def __init__(self, num_of_people):
+        self.num_of_people = num_of_people
+        self.bill = []
+
+    def order(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def get_subtotal(self):
+        pass
+
+    def split_bill(self):
+        pass
