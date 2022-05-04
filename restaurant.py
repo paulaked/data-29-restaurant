16e6167ee86a521801e11146274ab3d0148c17b6
Tatenda Mukutirwa <tatendanelson1@gmail.com>
2022-05-04 17:41:53 +0100

    Passes 3 tests

--- restaurant.py
+++ restaurant.py
@@ -8,10 +8,20 @@ class Table:
     def order(self, item, price, quantity=1):
         self.bill.append({"item": item, "price": price, "quantity": quantity})
 
-    def remove(self):
-        pass
+    def remove(self, item, price, quantity = 1):
+        for i in self.bill:
+            if i == self.bill:
+                return True
+            else:
+                return False
+
+
+
 
     def get_subtotal(self):
+        pass
+
+    def get_total(self):
         pass
 
     def split_bill(self):
