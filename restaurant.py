2a845576235927834c13df3ad39914d37fbffa94
Durgesh Murugan <durgeshmurugan7962@gmail.com>
2022-05-04 11:59:48 +0100

    update2

--- restaurant.py
+++ restaurant.py
@@ -1,15 +1,19 @@
 class Table:
 
-    def __init__(self, number_of_people, bill):
+    def __init__(self, number_of_people):
         self.number_of_people = number_of_people
-        self.bill = bill
+        self.bill = []
+        self.total = 0
 
     def order(self, item, price, quantity):
+        quantity = 1
+
         bill = dict.fromkeys(['item', 'price','quantity'])
         #orders = {"item": item, "price": price, "quantity": quantity}
         bill.append(item,price,quantity)
 
     def remove(self):
+        pass
 
 
     def get_subtotal(self):
