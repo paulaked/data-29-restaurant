6a1f068910c263a303ef1ddbc32ca531532abf1d
Durgesh Murugan <durgeshmurugan7962@gmail.com>
2022-05-04 15:01:58 +0100

    getsubtotal

--- restaurant.py
+++ restaurant.py
@@ -6,39 +6,43 @@ class Table:
         self.total = 0
 
     def order(self, item, price, quantity):
-        quantity = 1
         new_order = {"item": item, "price": price, "quantity": quantity}
-
+        quantity = 1
         if new_order not in self.bill:
             self.bill.append(new_order)
         return self.bill
-        #bill = dict.fromkeys(['item', 'price','quantity'])
 
-        #bill.append(item,price,quantity)
+    def remove(self, item, quantity):
 
-
-
-
-        #if item not in self.bill:
-        #bill.append(item)
-        #for i in self.bill
-
-    def remove(self):
+        #new_order = {"item": item, "price": price, "quantity": quantity}
         pass
+    #edit the bill
+    #returning true or false based on the bill
 
 
     def get_subtotal(self):
-        pass
-
+        total = 0
+        if self.bill == []:
+            return 0
+        else:
+            for i in self.bill:
+                total +=  i["price"] * i["quantity"]
+        return total
 
     def get_total(self):
+        
         pass
+    # return a diet with the keys : sub total (use get subtotal), service charge
+    # calculating the total for each price of item by quantiity
+    # total (subtotal + service charge)
+    # values returned to two decimal places
+
 
     def split_bill(self):
         pass
-
-
-
+    #subtotal/num of peoples
+    #
+    #
 
 
 
