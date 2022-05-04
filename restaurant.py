59e57bc1309b0ba4ba0420138d72b311a9b813dc
Rajvi Choksi <rajvichoksi@Rajvis-MacBook-Air.local>
2022-05-04 11:31:11 +0100

    updated restaurant activity

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,22 @@
 class Table:
-    pass
+
+    def __init__(self, tables, bill):
+        self.tables = tables
+        self.bill = list
+menu = {"item": item, "price": price, "quantity":quantity}
+
+    def order(self, quantity=1):
+        
+
+
+    def remove(self):
+
+
+    def get_subtotal(self):
+        return price * quanity
+
+
+    def get_total(self):
+
+
+    def split_bill(self):
