cdc49c8a284c996db84d26c460116bcf74418d8c
cex <cexhw@Louises-MBP.cust.communityfibre.co.uk>
2022-05-04 08:44:21 +0100

    updated restaurant task

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,26 @@
 class Table:
-    pass
+
+    def __init__(self, diners, bill): 
+        self.bill = bill          
+        self.diners = diners
+
+    def order(self, item, price, quantity):
+        menu_item = []
+       
+
+        quantity = 1
+        print(quantity)
+
+
+    def remove(self):
+        quantity = 
+        print()
+
+    def get_subtotal(self):
+        print()   
+
+    def get_total(self):
+        print()        
+
+
+
