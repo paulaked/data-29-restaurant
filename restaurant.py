7964ba19654ab0c97b013c6996600e8d3b1faedc
june <juneli@spartaglobal.com>
2022-05-04 11:46:56 +0100

    produce endless loop in order method

--- restaurant.py
+++ restaurant.py
@@ -7,20 +7,25 @@ class Table:
         self._subtotal = 0
 
     # method to append a menu item to the bill
-    # passes test_order and test_order_no_quantity
     def order(self, item, price, quantity = 1):
+
+        food_item = {"item": item, "price": price, "quantity": quantity}
+        amend_list = []
+        append_list = []
         if len(self.bill) > 0:
             for i in self.bill:
-                if item != i["item"]:
-                    self.bill.append({"item": item, "price": price, "quantity": quantity})
+                if i["item"] == food_item["item"] and i["price"] == food_item["price"]:
+                    i["quantity"] += food_item["quantity"]
                 else:
-                    i["quantity"] += quantity
+                    self.bill.append(i)
         else:
-            self.bill.append({"item": item, "price": price, "quantity": quantity})
+            self.bill.append(food_item)
+
+        return self.bill
+
 
 
     # method to remove bill items
-    # passes test_remove_no_item
     def remove(self, item, price, quantity = 1):
         if len(self.bill) != 0:
             for i in self.bill:
@@ -37,12 +42,20 @@ class Table:
     # get the total cost for the table based on quantities and prices in the bill
     def get_subtotal(self):
         bill_total = 0
-        item_total = 0
         if len(self.bill) != 0:
             for i in self.bill:
                 item_total = i["price"] * i["quantity"]
                 bill_total += item_total
-            self._subtotal = bill_total
-            return round(self._subtotal, 2)
+            self._subtotal = round(bill_total, 2)
+            return self._subtotal
         else:
             pass
+
+
+table02 = Table(2)
+
+table02.order('Food1', 10.00, 3)
+table02.order('Food2', 20.00, 1)
+table02.order('Food3', 0.50, 1)
+
+print(table02.bill)
