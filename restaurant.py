3cf661d7dfa39fbb24c2419d8e7f8d4fecc5ab32
Tatenda Mukutirwa <tatendanelson1@gmail.com>
2022-05-04 16:31:06 +0100

    Passed 2 tests

--- restaurant.py
+++ restaurant.py
@@ -1,16 +1,12 @@
 class Table:
 
+
     def __init__(self, num_of_people):
         self.num_of_people = num_of_people
         self.bill = []
 
     def order(self, item, price, quantity=1):
-        self.item = item
-        self.price = price
-        self.quantity
-
-    def order(self):
-        pass
+        self.bill.append({"item": item, "price": price, "quantity": quantity})
 
     def remove(self):
         pass
