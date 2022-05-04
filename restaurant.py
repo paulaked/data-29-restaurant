70f219fae76e649bbbb03fb12a93e60b491926b0
fahimul <fahimul.h.96@gmail.com>
2022-05-04 09:18:48 +0100

    restaurant

--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,8 @@
 class Table:
-    pass
+    def __init__(self):
+        self.order = order
+        self.remove = True
+        self.get_subtotal = get_subtotal
+        self.get_total = get_total
+        self.split_bill = split_bill
+
