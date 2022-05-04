0cb726c9cbc6ce3ad39c65f65a564f850854758f
fahimhasan1 <FHasan@spartaglobal.com>
2022-05-04 16:01:00 +0100

    improvements

--- restaurant.py
+++ restaurant.py
@@ -1,6 +1,3 @@
-import decimal
-
-
 class Table:
 
     def __init__(self,num):
@@ -9,41 +6,31 @@ class Table:
         self.total = 0
 
     def order(self, item, price, quantity=1):
-        a=False
-        p = 0
-        for i in self.bill:
-            if self.bill[p]["item"] == item and self.bill[p]["price"] == price:
-                self.bill[p]["quantity"] += 1
-                p+=1
+        for i in range(0,len(self.bill)):
+            if self.bill[i]["item"] == item and self.bill[i]["price"] == price:
+                self.bill[i]["quantity"] += 1
+
         self.bill.append({"item": item, "price" : price, "quantity": quantity})
 
-
-
     def remove(self, item, price, quantity):
-        p=0
-        for i in self.bill:
-            if self.bill[p]["item"] == item and self.bill[p]["price"]==price:
-                if self.bill[p]["quantity"] - int(quantity) >= 1:
-                    self.bill[p]["quantity"] -= int(quantity)
+        for i in range(0,len(self.bill)):
+            if self.bill[i]["item"] == item and self.bill[i]["price"]==price:
+                if self.bill[i]["quantity"] - int(quantity) >= 1:
+                    self.bill[i]["quantity"] -= int(quantity)
 
                 else:
-                    self.bill[p].clear()
-            p+=1
-
+                    self.bill[i].clear()
 
     def get_subtotal(self):
-        p=0
-        for i in self.bill:
-            self.total += (self.bill[p]["price"] * self.bill[p]["quantity"])
-            p+=1
+        for i in range(0,len(self.bill)):
+            self.total += round((self.bill[i]["price"] * self.bill[i]["quantity"]),2)
         return self.total
 
     def get_total(self, service):
         self.get_subtotal()
-        t= "{:.2f}".format(self.total)
         sc= self.total*service
         final_total= self.total + sc
-        total={"Sub Total": f'£{t}', "Service Charge": f'£{sc}', "Total":f'£{final_total}'}
+        total={"Sub Total": f'£{"{:.2f}".format(self.total)}', "Service Charge": f'£{sc}', "Total":f'£{final_total}'}
         return total
 
     def split_bill(self):
